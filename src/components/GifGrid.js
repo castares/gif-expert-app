96d97b43b7fe87e0d30bf2a1dc523b80fda6368d
César Castañón <cesar.castares@gmail.com>
2021-04-22 08:58:44 +0200

    [ADD] custom hook useFetchGifs + animated.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifGridItem } from "./GifGridItem";
-import { getGifs } from "../helpers/getGifs";
+// import { getGifs } from "../helpers/getGifs";
 
 require("dotenv").config();
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
+    const { data: images, loading } = useFetchGifs(category);
 
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category]);
+    console.log(loading);
 
     return (
         <>
-            <h3> {category} </h3>
+            <h3 className="animate__animated animate__fadeIn"> {category} </h3>
+
+            {loading && (
+                <p className="animate__animated animate__flash">Loading</p>
+            )}
             <div className="card-grid">
                 <ol>
                     {images.map((img) => (
